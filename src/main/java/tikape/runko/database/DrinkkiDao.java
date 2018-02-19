85aa31f41512ff632755a92198902b26e079e3b2
Huovila Eetu T M <hceetu@lx7-fuxi284.ad.helsinki.fi>
2018-02-19 14:27:10 +0200

    Drinkit

---
+++ src/main/java/tikape/runko/database/DrinkkiDao.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.domain.Drinkki;
+
+/**
+ *
+ * @author hceetu
+ */
+public class DrinkkiDao implements Dao{
+    
+    private Database database;
+
+    @Override
+    public Object findOne(Object key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Drinkki WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+        
+        rs.close();
+        stmt.close();
+        connection.close();
+        
+        return new Drinkki(rs.getInt("id"), rs.getString("nimi"));
+    }
+
+    @Override
+    public List findAll() throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Drinkki");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Drinkki> drinkit = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            drinkit.add(new Drinkki(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return drinkit;
+    }
+
+    @Override
+    public void delete(Object key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM Drinkki WHERE id = (?)");
+        PreparedStatement stmt2 = connection.prepareStatement("DELETE FROM AnnosRaakaAine WHERE annos_id = (?)");
+        stmt.setObject(1, key);
+        stmt2.setObject(1, key);
+
+        stmt.executeQuery();
+        stmt2.executeQuery();
+    }
+    
+}
